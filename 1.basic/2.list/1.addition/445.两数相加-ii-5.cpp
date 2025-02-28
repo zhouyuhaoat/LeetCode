a14da4150002066863346e054209feac12f0ab68
zhouyuhao <zhouyuhaoqin@qq.com>
2025-02-28 13:43:05 +0800

    List - Addition

---
+++ 1.basic/2.list/1.addition/445.两数相加-ii-5.cpp
@@ -0,0 +1,55 @@
+/*
+ *   author:    zhouyuhao
+ *   created:   2025-02-28 12:42:21
+ *   modified:  2025-02-28 12:51:09
+ *   project:   LeetCode of labuladong
+ *   venue:     914, Harbin
+ */
+
+/*
+ * @lc app=leetcode.cn id=445 lang=cpp
+ *
+ * [445] 两数相加 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        // pointer reversal <=> head insertion
+        ListNode *newHead = nullptr, *cur = head;
+        while (cur) {
+            ListNode* next = cur->next;
+            cur->next = newHead, newHead = cur; // head insert
+            cur = next;
+        }
+        return newHead;
+    }
+    ListNode* add(ListNode* l1, ListNode* l2) {
+        ListNode dummy, *cur = &dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            cur = cur->next = new ListNode(sum % 10);
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        return dummy.next;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverse(l1), l2 = reverse(l2);
+        return reverse(add(l1, l2));
+    }
+};
+// @lc code=end
