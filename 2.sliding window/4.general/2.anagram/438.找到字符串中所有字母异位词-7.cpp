4beae4e4a0fc6240000f6d931affd1245b6bdbf5
zhouyuhao <zhouyuhaoat@qq.com>
2026-01-26 11:45:35 +0800

    sliding window - general - anagram

---
+++ 2.sliding window/4.general/2.anagram/438.找到字符串中所有字母异位词-7.cpp
@@ -0,0 +1,35 @@
+/*
+ *   author:    zhouyuhao
+ *   created:   2025-02-12 23:01:03
+ *   modified:  2025-05-22 18:53:04
+ *   project:   LeetCode of labuladong
+ *   venue:     914, Harbin
+ */
+
+/*
+ * @lc app=leetcode.cn id=438 lang=cpp
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> res, cnt(128);
+        for (char ch : p) {
+            cnt[ch]++;
+        }
+        for (int lo = 0, hi = 0; lo < (int)s.size(); lo++) { // [lo, hi)
+            while (hi < (int)s.size() && cnt[s[hi]] > 0) { // not enough: have < need
+                cnt[s[hi++]]--;
+            } // enough: have >= need
+            if (hi - lo == (int)p.size()) { // just right: have = need
+                res.push_back(lo);
+            }
+            cnt[s[lo]]++;
+        }
+        return res;
+    }
+};
+// @lc code=end
---
+++ 2.sliding window/4.general/2.anagram/438.找到字符串中所有字母异位词-8.cpp
@@ -0,0 +1,35 @@
+/*
+ *   author:    zhouyuhao
+ *   created:   2025-02-12 23:01:03
+ *   modified:  2025-05-22 18:53:14
+ *   project:   LeetCode of labuladong
+ *   venue:     914, Harbin
+ */
+
+/*
+ * @lc app=leetcode.cn id=438 lang=cpp
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> res, cnt(128);
+        for (char ch : p) {
+            cnt[ch]++;
+        }
+        for (int lo = 0, hi = 0; hi < (int)s.size(); hi++) { // [lo, hi]
+            cnt[s[hi]]--;
+            while (cnt[s[hi]] < 0) { // too many: have > need
+                cnt[s[lo++]]++;
+            } // not too many: have <= need
+            if (hi - lo + 1 == (int)p.size()) { // just right: have = need
+                res.push_back(lo);
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
