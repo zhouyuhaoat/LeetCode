fe9b08d874cd0e4b7c921f097ae651a638cf6435
zhouyuhao <zhouyuhaoat@qq.com>
2026-01-29 14:52:51 +0800

    data structure - difference - easy

---
+++ 4.data structure/2.difference/3.easy/1094.拼车-2.cpp
@@ -0,0 +1,34 @@
+/*
+ *   author:    zhouyuhao
+ *   created:   2025-01-18 23:37:09
+ *   modified:  2026-01-28 22:42:13
+ *   project:   LeetCode of labuladong
+ *   venue:     914, Harbin
+ */
+
+/*
+ * @lc app=leetcode.cn id=1094 lang=cpp
+ *
+ * [1094] 拼车
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        map<int, int> seats;
+        for (auto& trip : trips) {
+            int num = trip[0], from = trip[1], to = trip[2];
+            seats[from] += num, seats[to] -= num;
+        }
+        int cap = 0;
+        for (auto [_, val] : seats) {
+            cap += val;
+            if (cap > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
